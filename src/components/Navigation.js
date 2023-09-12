8d227df9f8563d5ad008945451dfb7561bc3683e
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-12 02:06:32 -0400

    added navigation, pages

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import Container from "react-bootstrap/Container";
 import {Navbar, Nav} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
 
 function Navigation(){
     return(
@@ -10,16 +11,16 @@ function Navigation(){
                 <Navbar.Collapse id="navigation">
                     <Container>
                         <Nav>
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/bio">Biography</Nav.Link>
-                            <Nav.Link href="/education">Education</Nav.Link>
-                            <Nav.Link href="/experience">Work Experience</Nav.Link>
-                            <Nav.Link href="/qualities">Personal Qualities</Nav.Link>
-                            <Nav.Link href="/skills">Skills and Certifications</Nav.Link>
-                            <Nav.Link href="/development">Professional Development</Nav.Link>
-                            <Nav.Link href="/activities">Activities and Involvement</Nav.Link>
-                            <Nav.Link href="/hobbies">Hobbies</Nav.Link>
-                            <Nav.Link href="/contact">Contact</Nav.Link>
+                            <Nav.Link><NavLink to="/">Home</NavLink></Nav.Link>
+                            <Nav.Link><NavLink to="/bio">Biography</NavLink></Nav.Link>
+                            <Nav.Link><NavLink to="/education">Education</NavLink></Nav.Link>
+                            <Nav.Link><NavLink to="/experience">Work Experience</NavLink></Nav.Link>
+                            <Nav.Link><NavLink to="/qualities">Personal Qualities</NavLink></Nav.Link>
+                            <Nav.Link><NavLink to="/skills">Skills and Certifications</NavLink></Nav.Link>
+                            <Nav.Link><NavLink to="/development">Professional Development</NavLink></Nav.Link>
+                            <Nav.Link><NavLink to="/activities">Activities and Involvement</NavLink></Nav.Link>
+                            <Nav.Link><NavLink to="/hobbies">Hobbies</NavLink></Nav.Link>
+                            <Nav.Link><NavLink to="/contact">Contact</NavLink></Nav.Link>
                         </Nav>
                     </Container>
                 </Navbar.Collapse>
