714fce209206079568e108e2afcc46426bf75f18
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-11 22:23:56 -0400

    prepare for initial deployment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Routes, Route} from 'react-router';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
