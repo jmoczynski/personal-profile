cf45701a09961220fff868c965daccf30498f04c
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-11 22:35:52 -0400

    prepare for initial deployment

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<App/>}/>
-          </Routes>
-      </BrowserRouter>
-      <App/>
+      <HashRouter>
+          <App/>
+      </HashRouter>
   </React.StrictMode>
 );
 
