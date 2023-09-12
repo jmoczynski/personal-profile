b1722b9563ff5db48bc7021cca7c7ceb59eb8dfb
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-12 02:53:51 -0400

    worked on landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 function App() {
   return (
       <Container>
           <h1 className="text-center">Welcome</h1>
+          <p>Welcome to Justin Moczynski's personal website.</p>
+          <Image src={require("./static/images/senior-portrait.jpg")} fluid roundedCircle/>
       </Container>
   );
 }
