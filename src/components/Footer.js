ffdd119ddfede0eff43e2eb0c50b1312fd13b305
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-17 01:58:57 -0400

    worked on Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 function Footer(){
     return(
         <Container className="text-center">
-            <p>Modified 9/13/2023 by Justin Moczynski</p>
+            <p>Modified 9/17/2023 by Justin Moczynski</p>
         </Container>
     );
 }
