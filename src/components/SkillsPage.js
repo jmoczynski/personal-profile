088cc5721593dc24d18b01f1bbd77ae2e4517b8d
Justin Moczynski <jmoczynski@ithaca.edu>
2023-09-17 02:44:51 -0400

    worked on Skills page

--- src/components/SkillsPage.js
+++ src/components/SkillsPage.js
@@ -9,154 +9,154 @@ function SkillsPage(){
     const popover_Python = (
         <Popover id="popover_Python">
             <Popover.Header as="h3">Python</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for software, web application, and application exploitation development</Popover.Body>
         </Popover>
     );
 
     const popover_R = (
         <Popover id="popover_R">
             <Popover.Header as="h3">R</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for data analysis and linear algebra computations</Popover.Body>
         </Popover>
     );
 
     const popover_Java = (
         <Popover id="popover_Java">
             <Popover.Header as="h3">Java</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for software development</Popover.Body>
         </Popover>
     );
 
     const popover_HTML = (
         <Popover id="popover_HTML">
             <Popover.Header as="h3">HTML</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for web application development</Popover.Body>
         </Popover>
     );
 
     const popover_CSS = (
         <Popover id="popover_CSS">
             <Popover.Header as="h3">CSS</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for web application development</Popover.Body>
         </Popover>
     );
 
     const popover_JavaScript = (
         <Popover id="popover_JavaScript">
             <Popover.Header as="h3">JavaScript</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for web application development</Popover.Body>
         </Popover>
     );
 
     const popover_C = (
         <Popover id="popover_C">
             <Popover.Header as="h3">C</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for software and application exploitation development</Popover.Body>
         </Popover>
     );
 
     const popover_Assembly = (
         <Popover id="popover_Assembly">
             <Popover.Header as="h3">Assembly</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for software analysis and application exploitation development</Popover.Body>
         </Popover>
     );
 
     const popover_Tableau = (
         <Popover id="popover_Tableau">
             <Popover.Header as="h3">Tableau</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for data analysis</Popover.Body>
         </Popover>
     );
 
     const popover_Linux = (
         <Popover id="popover_Linux">
             <Popover.Header as="h3">Linux</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for learning about computer organization and systems and application exploitation development</Popover.Body>
         </Popover>
     );
 
     const popover_MySQL = (
         <Popover id="popover_MySQL">
             <Popover.Header as="h3">MySQL</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for database development</Popover.Body>
         </Popover>
     );
 
     const popover_SQLite = (
         <Popover id="popover_SQLite">
             <Popover.Header as="h3">SQLite</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for database development</Popover.Body>
         </Popover>
     );
 
     const popover_Docker = (
         <Popover id="popover_Docker">
             <Popover.Header as="h3">Docker</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for application exploitation development</Popover.Body>
         </Popover>
     );
 
     const popover_CPlusPlus = (
         <Popover id="popover_CPlusPlus">
-            <Popover.Header as="h3"></Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Header as="h3">C++</Popover.Header>
+            <Popover.Body>Used for software development</Popover.Body>
         </Popover>
     );
 
     const popover_Flask = (
         <Popover id="popover_Flask">
             <Popover.Header as="h3">Flask</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for web application development</Popover.Body>
         </Popover>
     );
 
     const popover_Pwntools = (
         <Popover id="popover_Pwntools">
             <Popover.Header as="h3">Pwntools</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for application exploitation development</Popover.Body>
         </Popover>
     );
 
     const popover_MicrosoftExcel = (
         <Popover id="popover_MicrosoftExcel">
             <Popover.Header as="h3">Microsoft Excel</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for data analysis</Popover.Body>
         </Popover>
     );
 
     const popover_ReactJS = (
         <Popover id="popover_ReactJS">
             <Popover.Header as="h3">ReactJS</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for web application development</Popover.Body>
         </Popover>
     );
 
     const popover_MongoDB = (
         <Popover id="popover_MongoDB">
             <Popover.Header as="h3">MongoDB</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for database development</Popover.Body>
         </Popover>
     );
 
     const popover_Ghidra = (
         <Popover id="popover_Ghidra">
             <Popover.Header as="h3">Ghidra</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for application exploitation development</Popover.Body>
         </Popover>
     );
 
     const popover_gdb = (
         <Popover id="popover_gdb">
             <Popover.Header as="h3">gdb</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for software and application exploitation development</Popover.Body>
         </Popover>
     );
 
     const popover_GoogleSheets = (
         <Popover id="popover_GoogleSheets">
             <Popover.Header as="h3">Google Sheets</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body>Used for data analysis</Popover.Body>
         </Popover>
     );
 
@@ -177,14 +177,14 @@ function SkillsPage(){
     const popover_CertExcelAssociate = (
         <Popover id="popover_CertExcelAssociate">
             <Popover.Header as="h3">Excel Associate</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body><p>No Expiration</p>Used for data analysis</Popover.Body>
         </Popover>
     );
 
     const popover_CertLifeguard = (
         <Popover id="popover_CertLifeguard">
             <Popover.Header as="h3">Lifeguarding</Popover.Header>
-            <Popover.Body>Description</Popover.Body>
+            <Popover.Body><p>Expires November 2024</p>Used for lifeguarding at aquatic facilities</Popover.Body>
         </Popover>
     );
 
